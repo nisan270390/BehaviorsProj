844809699a548515703e3c0b57909ccb5c8bb2cb
urielb92 <Urielb92@gmail.com>
2015-06-20 10:20:20 -0700

    Added map objects (basic stubs)

---
+++ BehaviorsProj/Map/Map.cpp
@@ -0,0 +1,24 @@
+/*
+ * Map.cpp
+ *
+ *  Created on: Jun 20, 2015
+ *      Author: colman
+ */
+
+#include "Map.h"
+
+Map::Map()
+{
+	// TODO
+}
+
+Map::UpdateMap()
+{
+	// TODO
+}
+
+Map::~Map()
+{
+	// TODO
+}
+
---
+++ BehaviorsProj/Map/Map.h
@@ -0,0 +1,22 @@
+/*
+ * Map
+ *
+ *  Created on: Jun 20, 2015
+ *      Author: colman
+ */
+
+#ifndef MAP_H_
+#define MAP_H_
+#include <libplayerc++/playerc++.h>
+
+class Map{
+public :
+	Map();
+	void UpdateMap();
+	virtual ~Map();
+};
+
+
+
+
+#endif /* MAP_ */
