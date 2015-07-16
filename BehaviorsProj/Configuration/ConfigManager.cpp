fdf8eeb00422c842b6b5248d45fffe83ea3fc757
TomerZv <TonerZv@gmail.com>
2015-07-16 10:10:48 -0700

    Add Config class

---
+++ BehaviorsProj/Configuration/ConfigManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * ConfigManager.cpp
+ *
+ *  Created on: Jul 16, 2015
+ *      Author: colman
+ */
+
+#include "ConfigManager.h"
+
+ConfigManager::ConfigManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ConfigManager::~ConfigManager() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ BehaviorsProj/Configuration/ConfigManager.h
@@ -0,0 +1,17 @@
+/*
+ * ConfigManager.h
+ *
+ *  Created on: Jul 16, 2015
+ *      Author: colman
+ */
+
+#ifndef CONFIGMANAGER_H_
+#define CONFIGMANAGER_H_
+
+class ConfigManager {
+public:
+	ConfigManager();
+	virtual ~ConfigManager();
+};
+
+#endif /* CONFIGMANAGER_H_ */
